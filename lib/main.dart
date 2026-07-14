c8eb36542445c2c0edfa56102ce4a6da713749b1
marceloterra73 <demagenciaon@gmail.com>
2026-07-14 12:40:08 -0300

    Adicionar url_strategy para navegacao web e remover async do script

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 ﻿import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:url_strategy/url_strategy.dart';
 import 'app/app.dart';
 import 'core/config/supabase_config.dart';
 import 'offline/seeders/bible_seeder.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  setPathUrlStrategy();
 
   // Inicializar Supabase (com placeholder - troque pelas credenciais reais)
   try {
